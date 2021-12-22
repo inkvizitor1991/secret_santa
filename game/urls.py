47489683f64aa8deef7fbfdc8700ba4df1cc2567
inkvizitor1991 <85788805+inkvizitor1991@users.noreply.github.com>
2021-12-22 17:39:12 +0300

    added button exit

--- game/urls.py
+++ game/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import BaseViews,RegistrationView,LoginView
+from django.contrib.auth.views import LogoutView
+from .views import BaseViews, RegistrationView, LoginView
 
 
 
 urlpatterns = [
     path('',BaseViews.as_view(), name='base'),
     path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path('registration/', RegistrationView.as_view(), name='registration'),
 
 ]
