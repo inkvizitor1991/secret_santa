29a3a1317924e4299055390d039b1d14bc8c9c2e
Cain Entropax Maxwell <cainmax@posteo.mx>
2021-12-23 18:02:52 +0300

    Refactoring create new models scheme

--- game/models.py
+++ game/models.py
@@ -1,5 +1,79 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth.models import User
+
+
+class Player(models.Model):
+    user = models.ForeignKey(
+            User,
+            related_name='user', verbose_name='Пользователь',
+            on_delete=models.CASCADE
+    )
+    name = models.CharField(max_length=100, verbose_name='Имя игрока')
+    email = models.EmailField(verbose_name='Почтовый адрес')
+    wishlist = models.TextField()
+    message_to_santa = models.TextField(blank=True)
+    gift_reciever = models.ForeignKey(
+            'self',
+            related_name='player_reciever',
+            null=True, blank=True,
+            on_delete=models.DO_NOTHING,
+    )
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Игрок'
+        verbose_name_plural = 'Игроки'
+
+
+class Game(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name='Название игры'
+    )
+    game_creator = models.ForeignKey(
+        User,
+        related_name='creator',
+        verbose_name='Создатель',
+        on_delete=models.CASCADE
+    )
+    game_creator_assistent = models.ManyToManyField(
+        Player,
+        related_name='creator_assistant',
+        verbose_name='Помощник',
+        null=True, blank=True,
+    )
+    price_limit = models.DecimalField(
+        max_digits=9, decimal_places=2,
+        default=1000, verbose_name='Стоимость подарка'
+    )
+    reg_date_limit = models.DecimalField(
+        default='2021-12-30',
+        verbose_name='Последний день регистрации'
+    )
+    draw_date = models.DateField(
+        default='2021-12-31',
+        verbose_name='Дата жеребьёвки'
+    )
+    gift_date = models.DateField(
+        verbose_name='Дата отправки подарка'
+    )
+    players = models.ManyToManyField(
+            Player,
+            related_name='games',
+            blank=True,
+    )
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Игра'
+        verbose_name_plural = 'Игры'
 
 
 
