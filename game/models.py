c5fa4342bd282afae9720442bdeaab57c5feeea9
Cain Entropax Maxwell <cainmax@posteo.mx>
2021-12-24 10:43:52 +0300

    Fix fields

--- game/models.py
+++ game/models.py
@@ -44,13 +44,13 @@ class Game(models.Model):
         Player,
         related_name='creator_assistant',
         verbose_name='Помощник',
-        null=True, blank=True,
+        blank=True,
     )
     price_limit = models.DecimalField(
         max_digits=9, decimal_places=2,
         default=1000, verbose_name='Стоимость подарка'
     )
-    reg_date_limit = models.DecimalField(
+    reg_date_limit = models.DateField(
         default='2021-12-30',
         verbose_name='Последний день регистрации'
     )
@@ -77,104 +77,104 @@ class Game(models.Model):
 
 
 
-class GameCreator(models.Model):
-    name = models.OneToOneField(
-        settings.AUTH_USER_MODEL,
-        verbose_name='Пользователь',
-        on_delete=models.CASCADE
-    )
-    email = models.EmailField(verbose_name='Почтовый адрес')
-
-    def __str__(self):
-        return self.name.username
-
-    class Meta:
-        verbose_name = 'Создатель игры'
-        verbose_name_plural = 'Создатели игры'
-
-
-class Game(models.Model):
-    name = models.CharField(
-        max_length=100,
-        verbose_name='Название игры'
-    )
-    game_creator = models.ForeignKey(
-        GameCreator, related_name='creator',
-        verbose_name='Создатель',
-        on_delete=models.CASCADE
-    )
-    price = models.DecimalField(
-        max_digits=9, decimal_places=2,
-        default=1000, verbose_name='Стоимость подарка'
-    )
-    draw_date = models.DateField(
-        default='2021-12-31',
-        verbose_name='Дата жеребьёвки'
-    )
-    gift_date = models.DateField(
-        verbose_name='Дата отправки подарка'
-    )
-    slug = models.SlugField()
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name = 'Игра'
-        verbose_name_plural = 'Игры'
-
-
-class Player(models.Model):
-    name = models.CharField(max_length=100, verbose_name='Имя участника')
-    slug = models.SlugField()
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name = 'Игрок'
-        verbose_name_plural = 'Игроки'
-
-
-class Registration(models.Model):
-    player_name = models.ForeignKey(
-        Player, related_name='player',
-        verbose_name='Игрок',
-        on_delete=models.CASCADE
-    )
-
-    interests = models.CharField(max_length=300, verbose_name='Интересы')
-    letter = models.TextField(verbose_name='Письмо санте')
-    slug = models.SlugField()
-
-    def __str__(self):
-        return self.player_name.name
-
-    class Meta:
-        verbose_name = 'Регистрация'
-        verbose_name_plural = 'Регистрации'
-
-
-class Draw(models.Model):
-    game = models.ForeignKey(
-        Game, related_name='game_discription',
-        verbose_name='Игра',
-        on_delete=models.CASCADE
-    )
-    giver = models.ForeignKey(
-        Player, related_name='gift_giver',
-        verbose_name='Кто дарит',
-        on_delete=models.CASCADE
-    )
-    recipient = models.ForeignKey(
-        Player, related_name='gift_recipient',
-        verbose_name='Кому дарит',
-        on_delete=models.CASCADE
-    )
-
-    # def __str__(self):
-    # Кто кому дарит
-
-    class Meta:
-        verbose_name = 'Жеребьевка'
-        verbose_name_plural = 'Жеребьевки'
+#class GameCreator(models.Model):
+#    name = models.OneToOneField(
+#        settings.AUTH_USER_MODEL,
+#        verbose_name='Пользователь',
+#        on_delete=models.CASCADE
+#    )
+#    email = models.EmailField(verbose_name='Почтовый адрес')
+#
+#    def __str__(self):
+#        return self.name.username
+#
+#    class Meta:
+#        verbose_name = 'Создатель игры'
+#        verbose_name_plural = 'Создатели игры'
+#
+#
+#class Game(models.Model):
+#    name = models.CharField(
+#        max_length=100,
+#        verbose_name='Название игры'
+#    )
+#    game_creator = models.ForeignKey(
+#        GameCreator, related_name='creator',
+#        verbose_name='Создатель',
+#        on_delete=models.CASCADE
+#    )
+#    price = models.DecimalField(
+#        max_digits=9, decimal_places=2,
+#        default=1000, verbose_name='Стоимость подарка'
+#    )
+#    draw_date = models.DateField(
+#        default='2021-12-31',
+#        verbose_name='Дата жеребьёвки'
+#    )
+#    gift_date = models.DateField(
+#        verbose_name='Дата отправки подарка'
+#    )
+#    slug = models.SlugField()
+#
+#    def __str__(self):
+#        return self.name
+#
+#    class Meta:
+#        verbose_name = 'Игра'
+#        verbose_name_plural = 'Игры'
+#
+#
+#class Player(models.Model):
+#    name = models.CharField(max_length=100, verbose_name='Имя участника')
+#    slug = models.SlugField()
+#
+#    def __str__(self):
+#        return self.name
+#
+#    class Meta:
+#        verbose_name = 'Игрок'
+#        verbose_name_plural = 'Игроки'
+#
+#
+#class Registration(models.Model):
+#    player_name = models.ForeignKey(
+#        Player, related_name='player',
+#        verbose_name='Игрок',
+#        on_delete=models.CASCADE
+#    )
+#
+#    interests = models.CharField(max_length=300, verbose_name='Интересы')
+#    letter = models.TextField(verbose_name='Письмо санте')
+#    slug = models.SlugField()
+#
+#    def __str__(self):
+#        return self.player_name.name
+#
+#    class Meta:
+#        verbose_name = 'Регистрация'
+#        verbose_name_plural = 'Регистрации'
+#
+#
+#class Draw(models.Model):
+#    game = models.ForeignKey(
+#        Game, related_name='game_discription',
+#        verbose_name='Игра',
+#        on_delete=models.CASCADE
+#    )
+#    giver = models.ForeignKey(
+#        Player, related_name='gift_giver',
+#        verbose_name='Кто дарит',
+#        on_delete=models.CASCADE
+#    )
+#    recipient = models.ForeignKey(
+#        Player, related_name='gift_recipient',
+#        verbose_name='Кому дарит',
+#        on_delete=models.CASCADE
+#    )
+#
+#    # def __str__(self):
+#    # Кто кому дарит
+#
+#    class Meta:
+#        verbose_name = 'Жеребьевка'
+#        verbose_name_plural = 'Жеребьевки'
